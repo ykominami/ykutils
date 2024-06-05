c43840f5652397342c8e324785c8c5de7ba773cf
ykominami <ykominami@gmail.com>
2024-06-05 20:19:15 +0900

    up

--- lib/ykutils.rb
+++ lib/ykutils.rb
@@ -28,7 +28,7 @@ require "ykutils/yamlxop"
 
 require "pathname"
 module Ykutils
-  TEST_DATA_DIR = "#{Pathname.new(__dir__).parent}test_data".freeze
+  TEST_DATA_DIR = Pathname.new(__dir__).parent + "test_data"
   # Your code goes here...
 
   class YkutilsError < StandardError
